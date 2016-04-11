206ebc35aeb18150a30858ccb454fb1625549490
yonelyvan <yonelyvan@gmail.com>
2016-04-11 11:32:27 -0500

    commit1

---
+++ comandos-git.sql
@@ -0,0 +1,18 @@
+git clone 'url'
+git commit -m "Commit message"
+
+--	git add filename  	-- un solo archivo -- carpeta?
+--	git add .
+
+git commit -m "Commit message"
+
+--envio de cambios
+git push origin master
+
+--actulaizar ultimo commit
+git pull 
+
+
+
+
+--mas ocmandos
